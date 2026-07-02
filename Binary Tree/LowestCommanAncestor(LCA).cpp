c7d21edb6fd13d3d5115e1ae462bd8649688c4af
Neha Agrawal <n.agrawal1309@gmail.com>
2026-07-02 11:06:58 +0530

    code of lca

--- Binary Tree/LowestCommanAncestor(LCA).cpp
+++ Binary Tree/LowestCommanAncestor(LCA).cpp
@@ -1,1 +1,34 @@
 
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+TreeNode* ans =NULL;
+int LCA(TreeNode* root, TreeNode* p, TreeNode* q){
+     if(root==NULL){
+            return 0;
+        }
+        int left=LCA(root->left,p,q);
+        int right=LCA(root->right,p,q);
+        int self=0;
+        if(root==p or root==q){
+            self=1;
+        }
+        int total=left+self+right;
+        if(total==2 and ans==NULL){
+            ans=root;
+        }
+        return total;
+}
+    TreeNode* lowestCommonAncestor(TreeNode* root, TreeNode* p, TreeNode* q) {
+        LCA(root,p,q);
+        return ans;
+    }
+};
